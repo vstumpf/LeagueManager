a0aae455bb2bcb0ca485a7d525ffe0d5a9049712
vstumpf <vincents.995@gmail.com>
2017-07-23 23:30:22 -0700

    initial commit of league manager

---
+++ src/main/java/com/vstumpf/LoLManager/LoLManagerApplication.java
@@ -0,0 +1,12 @@
+package com.vstumpf.LoLManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoLManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoLManagerApplication.class, args);
+	}
+}
